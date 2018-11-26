47432a8c83c56c8c534f0cb389939dda9ec8e282
Tonny <dev@tonny.org>
2018-11-25 19:19:16 -0800

    :heavy_plus_sign: Created custom rule for email validation (valid domain)

---
+++ app/Rules/Field/Email.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Rules\Field;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class Email implements Rule
+{
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $domain = substr(strrchr($value, "@"), 1);
+
+        return checkdnsrr($domain);
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return ':attribute must use a valid domain.';
+    }
+}
